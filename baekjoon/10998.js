a94e23634ab62370e2e21ccdd360fec9c7754876
박선희 <sunhee1029@hanmail.net>
2017-04-03 13:59:42 +0900

    https://www.acmicpc.net/problem/10998 A*B (node)

---
+++ baekjoon/10998.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+rl.on('line', function(line) {
+	var num = line.split(' ');
+	var a = parseInt(num[0]);
+	var b = parseInt(num[1]);
+	console.log(a * b);
+});
