9f422e76641633b8fdf0a6df3c1946a5c41b8e49
박선희 <sunhee1029@hanmail.net>
2017-04-03 14:14:47 +0900

    Add https://www.acmicpc.net/problem/10869 사칙연산 (node)

---
+++ baekjoon/10869.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+}); 
+rl.on('line', function(line) {
+	var num = line.split(' ');
+	var a = parseInt(num[0]);
+	var b = parseInt(num[1]);
+	console.log(a + b);
+	console.log(a - b);
+	console.log(a * b);
+	console.log(Math.floor(a / b));
+	console.log(a % b);
+	rl.close();
+});
