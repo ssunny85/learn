31bfd56b49cee54a0f334d66b98d0b30dd4b4f71
ssunny85 <ssssunny85@gmail.com>
2017-04-23 17:10:07 +0900

    Add TODOList App

---
+++ angularjs/TODOApp/controller.js
@@ -0,0 +1,33 @@
+var app = angular.module('todo',[]);
+
+app.controller('TodoCtrl',function($scope){
+	$scope.todos = [
+		{
+			title : '요가 수행',
+			completed : false,
+			createdAt : Date.now(),
+			id : 0
+		},
+		{
+			title : 'angular 학습',
+			completed : false,
+			createdAt : Date.now(),
+			id : 1
+		},
+		{
+			title : '운동하기',
+			completed : true,
+			createdAt : Date.now(),
+			id : 2
+		}
+	];
+
+	$scope.remove = function(todo){
+		var idx = $scope.todos.findIndex(function(item){
+			return item.id === todo.id;
+		});
+		if(idx > -1){
+			$scope.todos.splice(idx,1);
+		}
+	}
+});
