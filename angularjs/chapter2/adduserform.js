7364e49f9788bc1f158c6ab170c0df40989899b2
ssunny85 <ssssunny85@gmail.com>
2017-05-02 16:26:57 +0900

    Add formvaildation

---
+++ angularjs/chapter2/adduserform.js
@@ -0,0 +1,10 @@
+var app = angular.module('myapp',[]);
+
+app.controller('AddUserController',function($scope){
+	$scope.addUser = function(){
+		//가입된 사용자를 데이터베스이스에 저장하는 코드 작성
+	}
+	$scope.addUser = function(){
+		//전송버튼 클릭
+	}
+});
